72b23a57be57f47348ec9cf735a98bb4635ef28c
Thomas von Deyen <tvdeyen@gmail.com>
2011-07-24 18:09:03 +0200

    Fixed validation

--- app/models/order_decorator.rb
+++ app/models/order_decorator.rb
@@ -1,16 +1,8 @@
 Order.class_eval do
   
   attr_accessible :terms_conditions
-  validate :terms_confirmed?, :if => Proc.new{ |order| order.state == 'confirm' } && Proc.new{ |order| !order.skip_terms_confirmed_validation }
+  validates_confirmation_of :terms_conditions, :if => proc(){ |order| order.state == 'confirm' } && proc(){ |order| !order.skip_terms_confirmed_validation }
   
   attr_accessor :skip_terms_confirmed_validation
-  
-  def terms_confirmed?
-    if !state_changed? && !terms_conditions?
-      errors.add(:terms_conditions, :must_be_confirmed)
-      return false
-    end
-    return true
-  end
   
 end
